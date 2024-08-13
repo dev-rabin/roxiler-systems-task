84f736844cfd1d25f27cea69ad2e9908c6b8ca39
dev-rabin <robinmandhotia@gmail.com>
2024-08-13 17:59:47 +0530

    frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+      <>
+        <div className=" text-4xl text-center font-bold my-3">Roxiler task</div>
+      </>
+  );
+}
+
+export default App;
